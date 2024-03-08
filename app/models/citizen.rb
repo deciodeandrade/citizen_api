fe22a3536482041d638f179c55b4b1615ee7e797
Décio <deciocavalcantedeandrade@gmail.com>
2024-03-08 06:37:13 -0300

    Configurando db

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,13 +1,13 @@
 class Citizen < ApplicationRecord
   belongs_to :address
 
-  #has_one_attached :image
+  has_one_attached :image
 
   validates :full_name, presence: true
   validates :birth_date, presence: true, comparison: { less_than: Date.current }
   validates :phone, presence: true
   validates :status, presence: true
-  #validates :image, presence: true
+  validates :image, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :cpf, presence: true, uniqueness: true, cpf: true
   validates :cns, presence: true, uniqueness: true, cns: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_03_05_030328) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_05_030328) do
     t.string "status", default: "active", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "address_id"
+    t.bigint "address_id"
     t.index ["address_id"], name: "index_citizens_on_address_id"
   end
 
